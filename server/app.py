813c0291742cce8a63b8eb55767ad9844d1a5710
vundi <vundi4773@gmail.com>
2024-09-06 11:22:32 +0300

    cr lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,34 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_dict =[plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(plants_dict,200)
+        return response
+    
+    def post(self):
+        data =request.get_json()
+        new_plant = Plant(
+            name =data['name'],
+            image =data['image'],
+            price =data['price'],
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        response_dict = new_plant.to_dict()
+        return make_response(jsonify(response_dict),201)
+
+api.add_resource(Plants, '/plants')
+
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        plant =Plant.query.filter(Plant.id== id).first()
+        response = make_response(plant.to_dict(),200)
+        return response
         
+api.add_resource(PlantByID,'/plants/<int:id>')  
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -5,5 +5,7 @@ db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
-
     id = db.Column(db.Integer, primary_key=True)
+    name =  db.Column(db.String(80),nullable = False)
+    image = db.Column(db.String(225),nullable=True)
+    price = db.Column(db.Float, nullable =False)
